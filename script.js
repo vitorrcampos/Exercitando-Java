4abe771a405b9a81526637c552a59c83e9a6c778
joao vitor <jaun.campos04@gmail.com>
2022-01-14 15:25:40 -0300

    Desafio finalizado.

---
+++ script.js
@@ -0,0 +1,15 @@
+let string = "João"
+
+let numero = 17
+
+let ano = 2022
+
+console.log(2015)
+
+console.log("Estou mexendo no console.log")
+
+let quartaFeira = true
+
+console.log(quartaFeira)
+
+console.log(typeof quartaFeira)
